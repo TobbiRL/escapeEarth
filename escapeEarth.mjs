051837e408d06867dddaae10c54846b9ef0bb37a
TobbiRL <torbjornrl@uia.no>
2025-03-11 11:18:51 +0100

    attempts at getting an answer

--- escapeEarth.mjs
+++ escapeEarth.mjs
@@ -1,12 +1,12 @@
-/*
 import fetch from 'node-fetch';
-*/
 
 const URL = 'https://spacescavanger.onrender.com/';
 const SOLAR_URL = "https://api.le-systeme-solaire.net/en/";
 const MY_EMAIL = "torbjornrl@uia.no";
 
 const response = fetch("https://spacescavanger.onrender.com//start?player=torbjornrl@uia.no")
+const data = response.json();
+console.log("Start game" + data);
 
 /*
 fetch("Link", {
@@ -15,8 +15,8 @@ fetch("Link", {
     body: i,
 })
 */
-
+/*
 .then(response => response.json())
 .then(json => console.log(json))
 .catch(error => console.log(error))
-
+*/
